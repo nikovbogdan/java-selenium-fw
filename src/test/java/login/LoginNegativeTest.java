7034e3a0c0a63d51e2f22bef91b38fd167044d2d
Bogdan Nikov <84898273+nikovbogdan@users.noreply.github.com>
2022-02-07 16:03:46 +0200

    Polishing the code

--- src/test/java/login/LoginNegativeTest.java
+++ src/test/java/login/LoginNegativeTest.java
@@ -1,12 +1,9 @@
 package login;
 
 import base.BaseTest;
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 public class LoginNegativeTest extends BaseTest {
-    
-    WebDriver driver;
 
     @Test
     public void testLoginWithInvalidCredentials() {
